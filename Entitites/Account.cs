c68fd346b999c3fc33fc0ab4df0719514584d954
Vinicius 'Davina' Ferreira <viniciusferreira0708@gmail.com>
2022-04-25 11:47:14 -0300

    Main Class Account created

---
+++ Entitites/Account.cs
@@ -0,0 +1,35 @@
+﻿using Banker.Entities.Exceptions;
+
+namespace Bank.Entities
+{
+    abstract class Account
+    {
+        public int AccountNumber { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account() { }
+
+        public Account(int accountNumber, string holder, double balance)
+        {
+            AccountNumber = accountNumber;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            if (Balance - amount > 0.0)
+            {
+                throw new DomainException("Withdraw error: Insuficient balance");
+            }
+            // if the client tries to withdraw more money than they have, the action won't be allowed and it will show the error message instead.
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
