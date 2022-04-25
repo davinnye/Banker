7892bc271e319ebf249fe4bb780ee427a72ac33e
Vinicius 'Davina' Ferreira <viniciusferreira0708@gmail.com>
2022-04-25 11:46:14 -0300

    DomainException made

---
+++ Entitites/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Banker.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+        //When an exception happen to be found on the entities, they are going to throw them here. By saving such exceptions, it will be possible to show them without crashing the main application as well as allowing the user to understand what went wrong.
+    }
+}
