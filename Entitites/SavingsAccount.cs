5c10f38547190ca6eeb4e955a5b478cd21d1c6c7
Vinicius 'Davina' Ferreira <viniciusferreira0708@gmail.com>
2022-04-25 11:48:10 -0300

    Savings Account created

---
+++ Entitites/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace Bank.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int accountNumber, string holder, double balance, double interestRate) : base(accountNumber, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdatedBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+        //overrides the Withdraw method, adding an extra fee to the Savings Account withdraw.
+    }
+}
